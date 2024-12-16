b77b565edc08e2cf0336d36f02723bc468935a24
CUMT-Atom <ccatom@88.com>
2024-12-16 20:04:02 +0800

    Initial Commit

---
+++ FlyingKxq/FlyingKxqApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyingKxqApp.swift
+//  FlyingKxq
+//
+//  Created by atom on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct FlyingKxqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlyingKxqTests/FlyingKxqTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlyingKxqTests.swift
+//  FlyingKxqTests
+//
+//  Created by atom on 2024/12/16.
+//
+
+import Testing
+@testable import FlyingKxq
+
+struct FlyingKxqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
