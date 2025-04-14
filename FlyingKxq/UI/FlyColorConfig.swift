e7b4db0e5b4510020bcd246ff4a47e3a1c079834
CUMT-Atom <ccatom@88.com>
2025-04-14 14:42:48 +0800

    Revert "[feature]添加Markdown解析器与View"
    This reverts commit e7ee98e783cc068eaf6df83cd78b8a47fb1649db.

--- FlyingKxq/UI/FlyColorConfig.swift
+++ FlyingKxq/UI/FlyColorConfig.swift
@@ -20,30 +20,13 @@ extension Color {
     static let flyChipBackground = Color(dynamicLight: "#EBEDF0", dark: "#23242B")
 }
 
-fileprivate struct FlyDynamicColor {
-    let light: String
-    let dark: String
-}
-
 extension Color {
-    // 用于防止Color -> UIColor时丢失darkColor
-    private static var dynamicColorMap: [Color: FlyDynamicColor] = [:]
-    
     init(dynamicLight lightHex: String, dark darkHex: String) {
         self = Color(UIColor { traitCollection in
             let lightColor = UIColor(hex: lightHex)
             let darkColor = UIColor(hex: darkHex)
             return traitCollection.userInterfaceStyle == .dark ? darkColor : lightColor
         })
-        Color.dynamicColorMap[self] = FlyDynamicColor(light: lightHex, dark: darkHex)
-    }
-    
-    func toHex(colorScheme: ColorScheme = .light) -> String? {
-        if let dynamicColor = Color.dynamicColorMap[self] {
-            let result = colorScheme == .dark ? dynamicColor.dark : dynamicColor.light
-            return result
-        }
-        return nil
     }
 }
 
--- Pods/Target Support Files/Pods-FlyingKxq/Pods-FlyingKxq-frameworks.sh
+++ Pods/Target Support Files/Pods-FlyingKxq/Pods-FlyingKxq-frameworks.sh
@@ -177,7 +177,6 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Down/Down.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
@@ -185,7 +184,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Down/Down.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
